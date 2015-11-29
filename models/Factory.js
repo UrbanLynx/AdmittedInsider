eb3c45aebf7240e021c793ba13bba34302000ed2
Rakesh Karanth <rakeshgk1396@gmail.com>
2015-11-29 14:57:29 -0800

    Adding the GRE Card
    Adding the details for the GRE Card

--- models/Factory.js
+++ models/Factory.js
@@ -5,10 +5,13 @@ module.exports.getCardOfType = function (type){
   card.type = type;
   switch (type){
     case 'toefl': 
-      card.fields.push({content: {'type': 'input', 'text': 'University code', 'input': '92122'}});
+      card.fields.push({content: {'type': 'input', 'text': 'University Code', 'input': '92122'}});
       // card.fields.push({type: 'checked', text: 'I sent', checked: false});
       // card.fields.push({type: 'checked', text: 'University received', checked: false});
       // card.fields.push({type: 'date', text: 'Sent on', date: ''});
+      break;
+    case 'gre':
+      card.fields.push({content: {'type': 'input', 'text': 'University Code', 'input': '92122'}});
       break;
     default:
       cards.fields.push('nothing')
