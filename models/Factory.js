98a0af69488da3b0f96a2ae77baaa3e2c3084285
Stanislav <stasmushits@gmail.com>
2015-12-04 11:23:33 -0800

    Added (1) update card (2) delete card

--- models/Factory.js
+++ models/Factory.js
@@ -5,7 +5,7 @@ module.exports.getCardOfType = function (type){
   card.type = type;
   switch (type){
     case 'Toefl': 
-      card.fields.push({content: {'type': 'input', 'text': 'University Code', 'input': ''}});
+      card.fields.push({content: {type: 'input', text: 'University Code', input: ''}});
       //card.fields.push({type: 'checked', text: 'I sent', checked: false});
       //card.fields.push({type: 'checked', text: 'University received', checked: false});
       //card.fields.push({type: 'date', text: 'Sent on', date: ''});
@@ -25,10 +25,19 @@ module.exports.getCardOfType = function (type){
 }
 
 module.exports.getApplicationIndex = function(id, applications){
-  console.log('asked '+id)
+  console.log('asked application '+id)
   for (i=0; i< applications.length; i++){
-    console.log(i + " " + applications[i].id)
     if (applications[i].id == id){
+      return i;
+    }
+  }
+  return -1;
+}
+
+module.exports.getCardIndex = function(id, cards){
+  console.log('asked card '+id)
+  for (i=0; i< cards.length; i++){
+    if (cards[i].id == id){
       return i;
     }
   }
