2ee8b959a832ebc42f5b64cc3ac90487f4bf79ed
Weikang Chen <richardchen40@gmail.com>
2015-12-05 12:36:44 -0300

    Red card: -The card becomes more and more red everyday as the deadline gets closer. -Since we have datapicker now, we dont need that (mm/dd/yyyy) instruction.

--- public/js/overview.js
+++ public/js/overview.js
@@ -17,43 +17,26 @@ function doneGreen(){
 }
 
 function deadlineRed(){
-	var list = document.getElementsByName("universityDeadline");
-	//var list = document.getElementsByName("123");
+	var list = document.getElementsByName("deadlineDate");
+	var card = document.getElementsByName("card");
 	len = list.length;
 	for (i=0;i<len;i++){
 		var	myDate = new Date();
 		var mytime=myDate.toLocaleDateString();
-		var deadline = list[i].innerHTML;
-		var strArray = deadline.split("/");
-		//The format of Date is yyyy/mm/dd, but the format we use is mm/dd/yyyy, so we did this convertion
-		var deadlineDate = new Date(strArray[2],strArray[0]-1,strArray[1]);
+		var deadline = list[i].value;
+		var strArray = deadline.split("-");
 
+		var deadlineDate = new Date(strArray[0],strArray[1]-1,strArray[2]);
 		dMillionSeconds = deadlineDate - myDate;
 		dDay = dMillionSeconds/1000/3600/24;
 
-		
-		if (deadlineDate.getFullYear() != strArray[2] || deadlineDate.getMonth() != strArray[0]-1 || deadlineDate.getDate() != strArray[1]){
-			list[i].innerHTML = "(invalid time)";
-			list[i].style.cssText = "color: #FF0000!important;font-weight:bold";
-		}
-		else{
-			if(dDay > 7 && dDay < 30)
-				list[i].style.cssText = "color: #CC0000!important";
-			else if(dDay > 0 && dDay <= 7 )
-			    list[i].style.cssText = "color: #FF0000!important;font-weight:bold";
-			else
-			   	list[i].style.cssText = "color: #000000!important";
-		}
+		t = parseInt('FF',16).toString(10);
+		t = t*dDay/30;
+		t = parseInt(t,10).toString(16);
+
+		card[i].style.backgroundColor = "#FF"+t+t;
 	}
 }    
-
-function strDateTime(str) 
-{ 
-var r = str.match(/^(\d{1,2})(-|\/)(\d{1,2})\2(\d{1,4})$/); 
-if(r==null)return false; 
-var d= new Date(r[4], r[3]-1, r[1]); 
-return (d.getFullYear()==r[1]&&(d.getMonth()+1)==r[3]&&d.getDate()==r[4]); 
-} 
 
 deadlineRed();
 doneGreen();
